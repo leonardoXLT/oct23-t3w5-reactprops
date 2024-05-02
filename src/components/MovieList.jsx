372cbd526619701cbd880cbdf9aba075d4f0f0c2
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-05-02 20:30:59 +1000

    first commit

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,62 @@
+// ES6/ESM/module project import syntax
+import MovieCard from "./MovieCard.jsx";
+
+// CommonJS/NodeJS project import syntax
+// const MovieCard = require("./MovieCard.jsx");
+
+let favouriteMovies = [
+	"Aliens",
+	"Dune",
+	"Hot Fuzz",
+	"",
+	"Anchorman",
+	"Lock Stock and 2 Smoking Barrels",
+	"Shrek",
+	"Pokemon",
+	"",
+	"Blazing Saddles",
+	"Snatch",
+	"Shrek 2",
+	"The good, the bad, and the ugly",
+	"Robocop",
+	"",
+	"Bladerunner",
+	"Lord of the Rings 1-3"
+];
+
+export default function MovieList(){
+
+	return (
+	<section>
+		{favouriteMovies.map((movie, index) => {
+
+			// if (movie && movie.length === 0) {
+
+			// }
+
+			// if (movie.length !== 0 ){
+			// 	return <MovieCard key={movie + "-" + index} elephant={movie} />;
+			// }
+
+			if (movie.length === 0){
+				return null;
+			}
+
+			return <MovieCard key={movie + "-" + index} elephant={movie} />
+		})}
+	</section>
+	);
+}
+
+
+// export function SomeFunction(){
+
+// }
+
+// module.exports = {
+// 	SomeFunction
+// }
+
+// module.exports = MovieList;
+
+// import MovieList, {SomeFunction} from "thefile "
---
+++ src/pages/App.jsx
@@ -0,0 +1,23 @@
+import MovieList from '../components/MovieList';
+import '../stylesheets/App.css';
+
+function App(props) {
+
+  // 
+  // 
+
+  return (
+    <div className="App">
+      <h1>Favourite Movies</h1>
+
+      {/* <MovieList /> */}
+      {/* <MovieList /> */}
+      {props.children}
+
+      {/*  */}
+
+    </div>
+  );
+}
+
+export default App;
